5f54c381833aadd0dee3d7c006e277885f61243c
Abhishek Patil <106476263+abhi259@users.noreply.github.com>
2022-11-14 01:14:19 +0530

    template-update

--- src/Components/Projects/BlogWebsite/index.js
+++ src/Components/Projects/BlogWebsite/index.js
@@ -15,7 +15,7 @@ export default function BlogWebsite() {
     >
       <div data-aos="fade-right">
         <div className=" group  transition duration-700  ">
-          <div className=" md:w-[500px] relative">
+          <div className=" md:w-[500px] relative ">
             <div className="md:group-hover:-translate-x-14   md:dark:bg-[#05222a] bg-[#000000]  group-hover:bg-opacity-0  transition duration-700 ">
               <img
                 src="https://firebasestorage.googleapis.com/v0/b/blog-website-2-a2fb4.appspot.com/o/BlogWebsite.png?alt=media&token=9f31e90c-d01b-4147-900a-e9be5d3d6039"
@@ -23,7 +23,7 @@ export default function BlogWebsite() {
                 className=" mix-blend-screen object-cover  h-72  "
               />
             </div>
-            <div className=" absolute top-14 right-0 md:-right-52  md:w-[400px]   bg-[#929292]/60 dark:bg-[#001C23]/70 h-48 backdrop-blur-lg flex flex-col justify-around p-5 md:group-hover:translate-x-20 transition duration-700">
+            <div className=" absolute top-14 right-0 md:-right-52  md:w-[400px]   h-48  flex flex-col justify-around  bg-[#929292]/60 dark:bg-[#001C23]/60 backdrop-blur-xl  p-5 md:group-hover:translate-x-20 transition duration-700 ">
               <div>
                 <h1 className="text-[#32ABB4] text-xl pb-2">
                   <span className="text-2xl text-[#c7c7c7]">01.</span>{" "}
